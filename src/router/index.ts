7d4e61ea874317697b972c88d60957c16b727586
RGienz <devilukerenz19@gmail.com>
2026-05-25 21:17:29 +0800

    update 05252026

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import HomeRs from '../page/home/rsvp.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,6 +8,11 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      component: HomeRs,
+    },
+    {
+      path: '/test',
+      name: 'homes',
       component: HomeView,
     },
     {
